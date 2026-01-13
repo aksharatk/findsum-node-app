f11bd7e00d1815f7ed2ac87168c87140ddaf152c
aksharatk <aksharamanohar23@gmail.com>
2026-01-13 11:08:44 +0530

    Initial commit - Addded inde and package

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/findsum', (req, res) => {
+    let n1 = parseInt(req.query.n1);
+    let n2 = parseInt(req.query.n2);
+    res.send('sum ' + (n1 + n2));
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
